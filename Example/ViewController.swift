bbd0ed965c50380ca1e18d2068c9902c8955e919
Thomas Toffalori <thomas.toffalori@gmail.com>
2019-12-02 15:49:27 +0100

    Add example project

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -2,7 +2,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let appKey = "U2FsdGVkX1/UZ3hTvSua9SwVjntUujQ5tyXIGTm02MzIKyQFG3xrwOlG0IwBcVet2abe7ixe6H7lz90dkWuFJizrnKOwpGFjdsGQq9Vy/1+5MUwPUCXnB6rUgCFTlDaS"
+    // Insert your visionar AppKey here
+    let appKey = ""
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let vc = segue.destination as! ARViewController
         vc.modalPresentationStyle = .fullScreen
